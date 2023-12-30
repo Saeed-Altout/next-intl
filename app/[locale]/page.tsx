81192b45b9ce3003384f81dacc78c2fa9b20609b
Saeed-Altout <saeedaltoutpro@gmail.com>
2023-12-31 00:50:59 +0300

    update

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,19 +2,13 @@ import LocaleSwitcher from "@/components/LocaleSwitcher";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 
-export default function Home({
-  params: { locale },
-}: {
-  params: { locale: any };
-}) {
+export default function Home() {
   const t = useTranslations("IndexPage");
 
   return (
     <main>
       <h1>{t("title")}</h1>
-      <Link href="/about" locale={locale}>
-        go about
-      </Link>
+      <Link href="/about">go about</Link>
       <LocaleSwitcher />
     </main>
   );
